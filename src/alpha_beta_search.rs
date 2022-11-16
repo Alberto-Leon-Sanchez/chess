02bff720ab1e2a8d394e6f0c439c996c616424a8
Alberto <castor_cabron@pop-os.localdomain>
2022-11-16 21:55:09 +0100

    negamax

--- src/alpha_beta_search.rs
+++ src/alpha_beta_search.rs
@@ -1,3 +1,6 @@
+use std::char::MAX;
+use std::f32::MIN;
+
 use crate::game;
 use crate::move_gen;
 use crate::make_move;
@@ -9,23 +12,22 @@ static mut hash_access:u64 =0;
 static mut alpha_cuts:u64 = 0;
 static mut beta_cuts:u64 = 0;
 
-pub fn get_best(game:&game::GameInfo,depth:i8) -> move_gen::Move{
+pub fn get_best(game:&mut game::GameInfo,depth:i8) -> move_gen::Move{
     let mut best_move = move_gen::Move{origin:0,destiny:0,destiny_piece:piece::Piece::Empty,promotion:None};
     let mut best_score:i16 = -9999;
-    let mut game = game.clone();
 
-    let moves = move_gen::move_gen(&mut game);
+    let moves = move_gen::move_gen(game);
 
     for mut mov in moves{
-        make_move::make_move(&mut game,&mut mov);
-        let score = alpha_beta_max(i16::MIN,i16::MAX,depth-1,&mut game);
+        make_move::make_move(game,&mut mov);
+        let score = alpha_beta_max(i16::MIN,i16::MAX,depth-1,game);
 
         if score > best_score{
             best_score = score;
             best_move = mov;
         }
 
-        unmake::unmake_move(&mut game,mov);
+        unmake::unmake_move(game,mov);
     }
 
     println!("{},{}",unsafe {
@@ -33,6 +35,28 @@ pub fn get_best(game:&game::GameInfo,depth:i8) -> move_gen::Move{
     },unsafe {
         alpha_cuts
     });
+
+    best_move
+}
+
+pub fn get_best_negamax_alpha_beta(game:&mut game::GameInfo,depth:i8) -> move_gen::Move{
+    
+    let mut best_move = move_gen::Move{origin:0,destiny:0,destiny_piece:piece::Piece::Empty,promotion:None};
+    let mut best_score:i16 = -9999;
+
+    let moves = move_gen::move_gen(game);
+
+    for mut mov in moves{
+        make_move::make_move(game,&mut mov);
+        let score = negamax_alpha_beta(game,i16::MIN+1,i16::MAX-1,depth-1);
+
+        if score > best_score{
+            best_score = score;
+            best_move = mov;
+        }
+
+        unmake::unmake_move(game,mov);
+    }
 
     best_move
 }
@@ -104,6 +128,58 @@ fn order_moves(moves:&mut Vec<move_gen::Move>,scores:&Vec<i16>) {
     }
 }
 
+fn negamax_alpha_beta(game:&mut game::GameInfo,alpha:i16,beta:i16,depth:i8) -> i16{
+    if depth == 0{
+        return eval::eval(game);
+    }
+    let mut alpha = alpha;
+
+    let moves = move_gen::move_gen(game);
+
+    for mut mov in moves{
+        make_move::make_move(game,&mut mov);
+        let score = -negamax_alpha_beta(game,-beta,-alpha,depth-1);
+        unmake::unmake_move(game,mov);
+
+        if score >= beta{
+            return score;
+        }
+
+        if score > alpha{
+            alpha = score;
+        }
+    }
+    alpha
+}
+
+fn negamax_alpha_beta_with_memory(game:&mut game::GameInfo,alpha:i16,beta:i16,depth:i8) -> i16{
+    
+    let mut alpha = alpha;
+    let mut beta = beta;
+    let index:usize = (game.hash & game::TRANSPOSITION_TABLE_SIZE) as usize;
+
+    if game.transposition_table[index].zobrist_key == game.hash && game.transposition_table[index].depth >= depth{
+
+        unsafe{hash_access+=1}
+
+        match game.transposition_table[index].flag{
+            game::Flag::Exact => return game.transposition_table[index].value,
+            game::Flag::Lowerbound => alpha = std::cmp::max(alpha,game.transposition_table[index].value),
+            game::Flag::Upperbound => beta = std::cmp::min(beta,game.transposition_table[index].value),
+        }
+        if alpha >= beta{
+            return game.transposition_table[index].value;
+        }
+    }
+
+    let moves = move_gen::move_gen(game);
+
+    if depth == 0 || moves.len() == 0{
+        return eval::eval(game);
+    }
+
+    alpha
+}
 
 fn alpha_beta_max(alpha:i16,beta:i16,depth:i8,game:&mut game::GameInfo) -> i16 {
     if depth == 0{
--- src/bin/prueba.rs
+++ src/bin/prueba.rs
@@ -13,7 +13,7 @@ fn main(){
 
     let mut game = fen_reader::read_fen("6k1/pp2brp1/4Q2p/8/8/1PnrB3/P4PPP/R5K1 b - - 1 21");
     
-    let best_move = alpha_beta_search::get_best(&mut game, 6);
+    let best_move = alpha_beta_search::get_best_negamax_alpha_beta(&mut game, 5);
 
     println!("{},{}",best_move.origin,best_move.destiny);
     
